bb1a5a5cdacd3e5ea86aa9d750518afcf77a98a6
amalkurbanbaev <amalkurbanbaev@yandex.ru>
2025-06-02 13:41:51 +0500

    feat: internationalization

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,8 +1,12 @@
-export default function Home() {
+import { getTranslations } from "next-intl/server"
+
+export default async function Home() {
+  const t = await getTranslations("HomePage")
+
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
       <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
-        main
+        <h1>{t("title")}</h1>
       </main>
       <footer className="row-start-3 flex flex-wrap items-center justify-center gap-[24px]">
         footer
