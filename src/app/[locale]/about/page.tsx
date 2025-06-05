7448d5ce0e313bd7e4c89ed924f5d03c6367ce49
amalkurbanbaev <amalkurbanbaev@yandex.ru>
2025-06-05 23:18:46 +0500

    feat: about page

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,3 +1,20 @@
+import { AboutPageTemplate } from "@/templates/about"
+import { getTranslations } from "next-intl/server"
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ locale: "ru" | "en" }>
+}) {
+  const { locale } = await params
+  const t = await getTranslations({ locale, namespace: "about-page.meta" })
+
+  return {
+    title: t("title"),
+    description: t("description"),
+  }
+}
+
 export default function AboutPage() {
-  return <section>About Page</section>
+  return <AboutPageTemplate />
 }
---
+++ src/templates/about.tsx
@@ -0,0 +1,19 @@
+import { getTranslations } from "next-intl/server"
+
+export async function AboutPageTemplate() {
+  const t = await getTranslations("about-page.layout")
+
+  return (
+    <section className="container grid grid-cols-1 gap-y-8 py-14 md:grid-cols-3">
+      <h1 className="font-bold text-xl md:text-2xl">{t("title")}</h1>
+
+      <div className="whitespace-pre-line md:col-span-2 md:pr-14">
+        <p>
+          {t.rich("description", {
+            underline: (chunks) => <u className="underline">{chunks}</u>,
+          })}
+        </p>
+      </div>
+    </section>
+  )
+}
