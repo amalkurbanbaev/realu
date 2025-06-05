60952094420d202fd4acde8e394d6b22f547a95e
amalkurbanbaev <amalkurbanbaev@yandex.ru>
2025-06-06 00:29:59 +0500

    refactor: about page layout

--- src/templates/about.tsx
+++ src/templates/about.tsx
@@ -4,7 +4,7 @@ export async function AboutPageTemplate() {
   const t = await getTranslations("about-page.layout")
 
   return (
-    <section className="container grid grid-cols-1 gap-y-8 py-14 md:grid-cols-3">
+    <section className="container grid grid-cols-1 gap-y-4 py-8 md:grid-cols-3 md:py-14">
       <h1 className="font-bold text-xl md:text-2xl">{t("title")}</h1>
 
       <div className="whitespace-pre-line md:col-span-2 md:pr-14">
