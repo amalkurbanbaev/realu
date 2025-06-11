e6bd612f01681cf83105b9a2351bef92bc4eeedc
amalkurbanbaev <amalkurbanbaev@yandex.ru>
2025-06-11 20:21:20 +0500

    refactor: apple button as image

--- src/components/modules/apple-button.tsx
+++ src/components/modules/apple-button.tsx
@@ -8,17 +8,18 @@ export function AppleButton({
 }: ComponentPropsWithoutRef<"a">) {
   return (
     <a
-      className={cn(
-        "flex max-h-12 items-center justify-center gap-2 rounded-2xl bg-white px-4 py-3 text-sm",
-        className,
-      )}
+      className={cn(className)}
       href="https://www.apple.com"
       target="_blank"
       rel="noreferrer"
       {...props}
     >
-      <Image src="/apple-logo.svg" alt="apple logo" width={20} height={24} />
-      <span className="font-bold text-black">App Store</span>
+      <Image
+        src="/app-store-badge.svg"
+        alt="apple logo"
+        width={128}
+        height={48}
+      />
     </a>
   )
 }
