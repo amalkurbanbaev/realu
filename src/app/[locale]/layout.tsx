53b8aac643e57f62fb6abf52eb5d08c0ecc76c20
amalkurbanbaev <amalkurbanbaev@yandex.ru>
2025-06-11 20:20:59 +0500

    refactor: update fonts

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -3,7 +3,8 @@ import "@/styles/globals.css"
 import "lenis/dist/lenis.css"
 import { PageLayout } from "@/components/layout"
 import { routing } from "@/i18n/routing"
-import { geistMono, geistSans } from "@/styles/fonts"
+
+import { montserrat } from "@/styles/fonts"
 import { NextIntlClientProvider, hasLocale } from "next-intl"
 import { getMessages, getTranslations } from "next-intl/server"
 import { notFound } from "next/navigation"
@@ -48,9 +49,7 @@ export default async function RootLayout({
 
   return (
     <html lang={locale}>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${montserrat.variable} antialiased`}>
         <NextIntlClientProvider locale={locale} messages={messages}>
           <PageLayout>{children}</PageLayout>
         </NextIntlClientProvider>
