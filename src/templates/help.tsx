d2e7d4bfce6fb73e6d4e2a354bdaf8d327804e70
amalkurbanbaev <amalkurbanbaev@yandex.ru>
2025-06-15 17:15:16 +0500

    feat: slides particles

---
+++ src/templates/help.tsx
@@ -0,0 +1,64 @@
+import { Link } from "@/i18n/navigation"
+import { cn } from "@/lib/utils"
+import { ChevronDownIcon } from "lucide-react"
+import { getTranslations } from "next-intl/server"
+
+type HelpPageTemplate = {
+  activeTab?: string
+}
+
+const DEFAULT_TAB = "about_app"
+
+export async function HelpPageTemplate({
+  activeTab = DEFAULT_TAB,
+}: HelpPageTemplate) {
+  const t = await getTranslations("help-page")
+
+  const sections = t.raw("units") as Record<
+    string,
+    { title: string; questions: { title: string; text: string }[] }
+  >
+
+  const tabKeys = Object.keys(sections)
+
+  const section = sections[activeTab]
+
+  return (
+    <section className="container relative z-10 py-20 text-white">
+      <div className="grid grid-cols-[220px_1fr] gap-12">
+        <nav className="flex flex-col gap-4">
+          {tabKeys.map((key) => (
+            <Link
+              key={key}
+              href={`?tab=${key}`}
+              className={cn(
+                "text-2xl transition-colors",
+                key === activeTab
+                  ? "font-bold text-white"
+                  : "text-muted-foreground",
+              )}
+            >
+              {sections[key].title}
+            </Link>
+          ))}
+        </nav>
+
+        <div className="ml-auto w-full max-w-4xl space-y-4">
+          {section.questions.map((q, i) => (
+            // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+            <details key={i} className="group rounded-2xl bg-white/5">
+              <summary className="flex cursor-pointer list-none items-center justify-between p-6 font-semibold text-white">
+                <span>{q.title}</span>
+                <ChevronDownIcon className="transition-transform duration-300 group-open:rotate-180" />
+              </summary>
+
+              <div className="px-6 pb-6">
+                <p className="text-white/80 leading-relaxed">{q.text}</p>
+              </div>
+            </details>
+          ))}
+        </div>
+      </div>
+    </section>
+  )
+}
--- src/templates/slides.tsx
+++ src/templates/slides.tsx
@@ -1,49 +1,32 @@
 "use client"
 
+import { Particle } from "@/components/modules/particle"
 import { useScrollAnimation } from "@/hooks"
+import { useSlides } from "@/hooks/use-slides"
 import { cn } from "@/lib/utils"
 import gsap from "gsap"
 import { ScrollTrigger } from "gsap/ScrollTrigger"
-import { useLocale, useTranslations } from "next-intl"
 import Image from "next/image"
 import { useRef } from "react"
 
 gsap.registerPlugin(ScrollTrigger)
 
-const imageKeys = [
-  "learn",
-  "meditation",
-  "breath",
-  "yoga",
-  "tests",
-  "watch",
-  "warmup",
-]
-
 export function SlidesOverlay() {
-  const t = useTranslations("home-page.layout")
-  const locale = useLocale()
-
   const sectionRefs = useRef<(HTMLElement | null)[]>([])
   const imageRefs = useRef<(HTMLDivElement | null)[]>([])
 
-  const slides = imageKeys.map((key, index) => ({
-    key,
-    title: t(`slides.${index}.title`),
-    description: t(`slides.${index}.description`),
-    image: `/screenshots/${locale}/${key}.png`,
-  }))
+  const slides = useSlides()
 
   const { activeIndex } = useScrollAnimation(sectionRefs, imageRefs)
 
   return (
-    <section className="container">
+    <section>
       <div className="relative grid h-full grid-cols-3 gap-4">
         {/* Панелька */}
-        <div className="sticky top-0 z-10 flex h-screen flex-col items-start justify-center gap-4">
+        <div className="container sticky top-0 z-10 flex h-screen flex-col items-start justify-center gap-4">
           {slides.map((slide, i) => (
             <button
-              key={slide.key}
+              key={slide.id}
               type="button"
               onClick={() =>
                 sectionRefs.current[i]?.scrollIntoView({
@@ -67,7 +50,7 @@ export function SlidesOverlay() {
         <div className="col-span-2 flex flex-col scroll-smooth">
           {slides.map((slide, i) => (
             <div
-              key={slide.key}
+              key={slide.id}
               data-index={i}
               ref={(el) => {
                 sectionRefs.current[i] = el
@@ -84,17 +67,62 @@ export function SlidesOverlay() {
                 <Image
                   src={slide.image}
                   alt={slide.title}
-                  width={300}
-                  height={600}
+                  width={279}
+                  height={606}
                   className="rounded-xl shadow-xl"
                 />
               </div>
 
+              {/* Градиент */}
+              <div
+                className={cn(
+                  "-z-10 pointer-events-none absolute inset-0 select-none",
+                )}
+              >
+                <Image
+                  src={`/gradients/${slide.id}.png`}
+                  alt="bg-slide-gradient"
+                  fill
+                />
+              </div>
+
               {/* Справа: текст */}
-              <div className="flex max-w-sm items-center p-4">
+              <div className="relative flex max-w-sm items-center p-4">
                 <div className="font-medium text-white/80 text-xl">
                   {slide.description}
                 </div>
+
+                {slide.particles.map((p) => (
+                  <Particle
+                    key={p.src}
+                    width={p.width}
+                    height={p.height}
+                    src={p.src}
+                    alt={p.src}
+                    className={cn(p.position)}
+                  />
+                ))}
+
+                {/* <div className="absolute top-0 left-0 size-full border">
+                  <Image fill alt="particle" src="/particles/learn-1.svg" />
+                </div> */}
+
+                {/* <div className="absolute inset-x-0 border-2">
+                  {slide.particles.map((p, i) => (
+                    <div
+                      key={p}
+                      className={cn(
+                        // "absolute inset-x-0 size-full",
+                        i % 2
+                          ? "translate-y-10"
+                          : "-translate-y-1/2 top-0 h-20",
+                        "h-full w-full border border-red-500",
+                      )}
+                    >
+                      <Image src={p} alt="particle" fill />
+                    </div>
+                  ))}
+                </div> */}
               </div>
             </div>
           ))}
