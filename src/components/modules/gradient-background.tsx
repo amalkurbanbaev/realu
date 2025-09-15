a41a5ee9e18935d1c1975e8c000035ccd389b0c4
amalkurbanbaev <amalkurbanbaev@yandex.ru>
2025-09-15 11:43:40 +0500

    feat: top gradient

--- src/components/modules/gradient-background.tsx
+++ src/components/modules/gradient-background.tsx
@@ -11,7 +11,7 @@ export function GradientBackground({ position }: GradientBackgroundProps) {
     <div
       className={cn(
         "pointer-events-none absolute inset-x-0 flex",
-        position === "top" ? "top-0 z-0 h-[calc(100vh/2)] w-full" : "-z-10 bottom-0 h-[300px] w-full object-cover object-bottom",
+        position === "top" ? "top-0 z-0 h-[calc(100vh/1.5)] w-full" : "-z-10 bottom-0 h-[300px] w-full object-cover object-bottom",
       )}
     >
       <Image src={position === "top" ? "/gradients/header-gradient.png" : "/gradients/footer-gradient.png"} alt={`gradient-${position}`} fill />
