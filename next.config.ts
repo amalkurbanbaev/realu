ba2a35f5159d4855b86e4c5c756c4fc61910fb73
amalkurbanbaev <amalkurbanbaev@yandex.ru>
2025-12-28 20:49:57 +0500

    chore: allow dev origins

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next"
 import createNextIntlPlugin from "next-intl/plugin"
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+  // Разрешаем доступ с других устройств в локальной сети
+  allowedDevOrigins: ["192.168.1.127", "localhost", "127.0.0.1"],
+}
 
 const withNextIntl = createNextIntlPlugin()
 export default withNextIntl(nextConfig)
